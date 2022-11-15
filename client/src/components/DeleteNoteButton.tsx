66fb61e821e7154cc4143280f87e40050ca5fa9b
hermancai <30858461+hermancai@users.noreply.github.com>
2022-11-15 04:26:23 -0800

    Add delete confirm dialog

--- client/src/components/DeleteNoteButton.tsx
+++ client/src/components/DeleteNoteButton.tsx
@@ -6,18 +6,35 @@ import {
   deleteNote,
   removeNoteFromList,
 } from "../features/note/noteSlice";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export default function DeleteNoteButton(props: { id: number }) {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
-  const handleClick = async () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
     try {
       await dispatch(deleteNote(props.id)).unwrap();
       dispatch(removeNoteFromList({ id: props.id }));
       dispatch(resetNote());
+      handleClose();
       navigate("/");
     } catch (err) {
       console.log(err);
@@ -25,8 +42,36 @@ export default function DeleteNoteButton(props: { id: number }) {
   };
 
   return (
-    <Button variant="text" onClick={handleClick} color="error">
-      DELETE
-    </Button>
+    <div>
+      <Button variant="text" onClick={handleOpen} color="error">
+        DELETE
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-describedby="delete-confirm-description"
+      >
+        <DialogContent sx={{ padding: "1rem" }}>
+          <DialogContentText id="delete-confirm-description">
+            Delete this note?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            justifyContent: "space-between",
+            minWidth: { sm: "400px" },
+            padding: "0 1rem 1rem 1rem",
+            gap: "2rem",
+          }}
+        >
+          <Button variant="outlined" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="contained" color="error" onClick={handleDelete}>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
--- client/src/routes/AccountPage.tsx
+++ client/src/routes/AccountPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { AppDispatch, RootState } from "../app/store";
 import { useSelector, useDispatch } from "react-redux";
+import { deleteAccount, verifyAccessToken } from "../features/user/userSlice";
 import {
-  deleteAccount,
-  setUsername,
-  verifyAccessToken,
-} from "../features/user/userSlice";
-import { Box, Button } from "@mui/material";
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export default function AccountPage() {
@@ -14,6 +17,16 @@ export default function AccountPage() {
   const navigate = useNavigate();
 
   const { username } = useSelector((state: RootState) => state.user);
+
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   React.useEffect(() => {
     const verifyUser = async () => {
@@ -25,9 +38,9 @@ export default function AccountPage() {
       }
     };
     verifyUser();
-  }, []);
+  }, [dispatch, navigate]);
 
-  const handleClick = async () => {
+  const handleDeleteAccount = async () => {
     try {
       await dispatch(deleteAccount()).unwrap();
       navigate("/login");
@@ -39,13 +52,47 @@ export default function AccountPage() {
   return (
     <Box>
       <p>Username: {username}</p>
-      <Button
-        variant="contained"
-        onClick={handleClick}
-        disabled={!username || username === "Guest"}
-      >
-        Delete Account
-      </Button>
+      <div>
+        <Button
+          variant="contained"
+          onClick={handleOpen}
+          disabled={!username || username === "Guest"}
+          color="error"
+        >
+          Delete Account
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-describedby="delete-confirm-description"
+        >
+          <DialogContent sx={{ padding: "1rem" }}>
+            <DialogContentText id="delete-confirm-description">
+              Permanently delete this account?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions
+            sx={{
+              justifyContent: "space-between",
+              minWidth: { sm: "400px" },
+              padding: "0 1rem 1rem 1rem",
+              gap: "2rem",
+            }}
+          >
+            <Button variant="outlined" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleDeleteAccount}
+              sx={{ margin: "0" }}
+            >
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     </Box>
   );
 }
